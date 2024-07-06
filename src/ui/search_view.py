1686f99bcc3ebc55651817c8360e2255d04f0d7d
Ernesto <ernest.ponce92@gmail.com>
2024-07-06 08:25:34 -0700

    Refactor dependencies and update versions in requirements.txt

--- src/ui/search_view.py
+++ src/ui/search_view.py
@@ -23,7 +23,14 @@ class SearchView(ft.UserControl):
             "Scrape",
             on_click=self.handle_search,
             height=self.input_height,
-            icon=ft.icons.SEARCH,  
+            style=ft.ButtonStyle(
+                shape=ft.RoundedRectangleBorder(radius=8),
+            ),
+        )
+        self.scan_all_button = ft.ElevatedButton(
+            "Scan All Products",
+            on_click=self.handle_scan_all,
+            height=self.input_height,
             style=ft.ButtonStyle(
                 shape=ft.RoundedRectangleBorder(radius=8),
             ),
@@ -38,7 +45,9 @@ class SearchView(ft.UserControl):
             ft.Row([
                 self.search_term,
                 ft.Container(width=10),  # Add spacing between field and button
-                self.search_button
+                self.search_button,
+                ft.Container(width=10),  # Add spacing between buttons
+                self.scan_all_button
             ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
             self.working_text,
             self.progress_bar,
@@ -64,6 +73,22 @@ class SearchView(ft.UserControl):
             await self.update_async()
         else:
             print("Please enter a search term")
+
+    async def handle_scan_all(self, e):
+        self.progress_text.visible = True
+        self.progress_bar.visible = True
+        self.checkmark.visible = False
+        self.working_text.value = "Scanning all products..."
+        self.working_text.visible = True
+        self.progress_bar.value = 0  # Reset progress bar
+        await self.update_async()
+        products = await self.scrape_all_products(self.update_progress)
+        await self.search_callback(products)  # Await the callback
+        self.progress_bar.visible = False
+        self.progress_text.visible = False
+        self.checkmark.visible = True
+        self.working_text.visible = False
+        await self.update_async()
 
     async def update_progress(self, current, total):
         self.progress_bar.value = current / total
@@ -140,6 +165,87 @@ class SearchView(ft.UserControl):
         # Save the data in a CSV file
         keys = products[0].keys()
         file_path = 'products.csv'
+        try:
+            with open(file_path, 'w', newline='', encoding='utf-8') as output_file:
+                dict_writer = csv.DictWriter(output_file, fieldnames=keys)
+                dict_writer.writeheader()
+                dict_writer.writerows(products)
+            print(f"File saved successfully at {os.path.abspath(file_path)}")
+        except Exception as e:
+            print(f"Error saving file: {e}")
+
+        return products
+
+    async def scrape_all_products(self, progress_callback):
+        service = Service(ChromeDriverManager().install())
+        options = webdriver.ChromeOptions()
+        options.add_argument('--headless')
+        driver = webdriver.Chrome(service=service, options=options)
+
+        url = 'https://www.supermarket23.com/en/productos'
+        driver.get(url)
+
+        WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single_product')))
+
+        total_results_element = driver.find_element(By.XPATH, '//p[contains(text(),"Showing")]')
+        total_results_text = total_results_element.text
+        total_results = int(total_results_text.split()[-2])
+        results_per_page = 20
+        total_pages = (total_results // results_per_page) + 1
+
+        products = []
+        progress_total_steps = total_pages * results_per_page
+        progress_current_step = 0
+
+        for page in range(1, total_pages + 1):
+            if page > 1:
+                page_url = f"{url}?pagina={page}"
+                driver.get(page_url)
+                WebDriverWait(driver, 10).until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'single_product')))
+            
+            product_elements = driver.find_elements(By.CLASS_NAME, 'single_product')
+
+            for product in product_elements:
+                try:
+                    name_element = WebDriverWait(product, 10).until(EC.presence_of_element_located((By.CSS_SELECTOR, 'h3 > a')))
+                    price_element = product.find_elements(By.CSS_SELECTOR, 'span.current_price')
+                    if not price_element:
+                        price_element = product.find_elements(By.CSS_SELECTOR, 'span.regular_price')
+                    old_price_element = product.find_elements(By.CSS_SELECTOR, 'span.old_price')
+                    price_by_weight_element = product.find_elements(By.CSS_SELECTOR, 'span.price-by-weight')
+                    
+                    name = name_element.get_attribute('innerText').strip() if name_element else "No name found"
+                    price = price_element[0].get_attribute('innerText').strip() if price_element else "No price found"
+                    old_price = old_price_element[0].get_attribute('innerText').strip() if old_price_element else "No old price found"
+                    price_by_weight = price_by_weight_element[0].get_attribute('innerText').strip() if price_by_weight_element else "No price by weight found"
+                    link = product.find_element(By.CSS_SELECTOR, 'div.product_thumb > a').get_attribute('href')
+                    
+                    product_number = link.split('/')[-1]
+                    image_url = f"https://medias.treew.com/imgproducts/middle/{product_number}.jpg"
+                    print(f"Product: {name}, Image URL: {image_url}")  # Debug message
+                    
+                    product_info = {
+                        'name': name,
+                        'price': float(price.replace('$', '').replace(',', '')),
+                        'old_price': float(old_price.replace('$', '').replace(',', '')) if old_price != "No old price found" else None,
+                        'price_by_weight': price_by_weight,
+                        'link': link,
+                        'image_url': image_url
+                    }
+                    products.append(product_info)
+                except Exception as e:
+                    print(f"Error processing product: {e}")
+                
+                # Update progress more frequently
+                progress_current_step += 1
+                await progress_callback(progress_current_step, progress_total_steps)
+                await asyncio.sleep(0.01)  # Small sleep to allow UI update
+
+        driver.quit()
+
+        # Save the data in a CSV file
+        keys = products[0].keys()
+        file_path = 'all_products.csv'
         try:
             with open(file_path, 'w', newline='', encoding='utf-8') as output_file:
                 dict_writer = csv.DictWriter(output_file, fieldnames=keys)
